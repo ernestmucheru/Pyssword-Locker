dba2b12251062d572ea73796c938f998040caf07
Your Name <you@example.com>
2021-05-28 13:53:35 +0300

    created conditionals for option 1 new user

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from user import User
 
 def main():
     while True:
-        print("Welcome to Siri, your password locker/n")
+        print("Welcome to Siri, your password locker \n")
         print("If you are new to siri, press '1' : If you are already a Siri member, press '2' : to exit siri, press '0'")
         short_code = input().str()
 
@@ -16,9 +16,30 @@ def main():
         print("Confirm Password")
         confirm_password = input()
 
-    while confirm_password != created_new_password:
-        print("Passwords DO NOT match")
-        print("Recreate your Password")
-        created_user_password = input()
-        print("Confirm Password")
-        confirm_password = input()
+        while confirm_password != created_new_password:
+            print("Passwords DO NOT match")
+            print("Recreate your Password")
+            created_user_password = input()
+            print("Confirm Password")
+            confirm_password = input()
+        
+        else:
+            print(f"Hoooray! {created_user_name}. You have account has been successfully created! \n")
+            print("Proceed to login to your account")
+            print("Username")
+            entered_username = input()
+            print("Password")
+            entered_password = input()
+
+
+            while entered_username != created_user_name or entered_password != created_user_password:
+                print("Invalid login credentials\n")
+                print("Username")
+                entered_username = input()
+                print("Password")
+                entered_password = input()
+            
+            else:
+                print(f"Welcome {entered_username} to your Siri account\n")
+
+    
