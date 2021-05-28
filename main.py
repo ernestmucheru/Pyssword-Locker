5954b6b19309901b9fd636b78296e84c7760329f
Your Name <you@example.com>
2021-05-28 12:55:40 +0300

    created the entry info for the application

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from user import User
+
+def main():
+    while True:
+        print("Welcome to Siri, your password locker/n")
+        print("If you are new to siri, press '1' : If you are already a Siri member, press '2' : to exit siri, press '0'")
+        short_code = input().str()
+
+        
