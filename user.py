e2ebd12d5a62d21ae43ce228a2a7a4afc794ba63
Your Name <you@example.com>
2021-05-28 12:27:25 +0300

    defined an init method to aid us in defining properties for our users

--- user.py
+++ user.py
@@ -4,3 +4,10 @@ class Users
     '''
     user_list = []
     
+    def __init__(self,user_name,password)
+    '''
+    __init__ method will help us define properties for our objects
+    Arguments:
+        user_name: The user's username.
+        password: The user's login pass to the locker.
+    '''
