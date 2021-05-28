b201d502bd9bd9f8c43aae13f1696bdc2889f0eb
Your Name <you@example.com>
2021-05-28 12:32:33 +0300

    created our instance variables which will be unique to every new instance of the class

--- user.py
+++ user.py
@@ -1,13 +1,18 @@
-class Users
+class User:
     '''
     This is a class that will generate a new instance of the user
     '''
     user_list = []
     
-    def __init__(self,user_name,password)
+    def __init__(self,user_name,password):
+    
+        self.user_name = user_name
+        self.password = password
+
     '''
-    __init__ method will help us define properties for our objects
+    __init__ method will help us define properties for our user
     Arguments:
         user_name: The user's username.
         password: The user's login pass to the locker.
     '''
+    
