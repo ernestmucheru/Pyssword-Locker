65c7d24872100afe182c7adc69f815bf51de47dd
Your Name <you@example.com>
2021-05-28 12:58:37 +0300

    created user registration input form

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ def main():
         print("If you are new to siri, press '1' : If you are already a Siri member, press '2' : to exit siri, press '0'")
         short_code = input().str()
 
-        
+    if short_code == '1':
+        print("Create Username")
+        created_user_name = input()
+
+        print("Create a Password")
+        created_user_password = input()
+
+        print("Confirm Password")
+        confirm_password = input()
