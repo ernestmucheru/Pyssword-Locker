34687849663cb9366fcee03226969380b9adea7e
Your Name <you@example.com>
2021-05-31 06:19:49 +0300

    create function to display credentials saved by a user

--- main.py
+++ main.py
@@ -42,6 +42,12 @@ def save_credential(credential):
 	'''
 	Credential.save_credentials(credential)
 
+def display_credentials(user_name):
+	'''
+	Function to display credentials saved by a user
+	'''
+	return Credential.display_credentials(user_name)
+
 
 
 
