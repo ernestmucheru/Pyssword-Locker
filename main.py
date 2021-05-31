2ea9624fd8755b99ec30520462257b5dfa941721
Your Name <you@example.com>
2021-05-31 06:20:40 +0300

    create a function to copy cedential details to clipboard

--- main.py
+++ main.py
@@ -48,6 +48,11 @@ def display_credentials(user_name):
 	'''
 	return Credential.display_credentials(user_name)
 
+def copy_credential(site_name):
+	'''
+	Function to copy a credentials details to the clipboard
+	'''
+	return Credential.copy_credential(site_name)
 
 
 
